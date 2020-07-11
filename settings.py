3a89b96c678262ac85798485ade3f90227b47c25
Lucky_Cat <60096074+Be3y4uu-K0T@users.noreply.github.com>
2020-07-12 00:13:20 +0500

    Настройка бота и датабаза вместо старого info.json

---
+++ settings.py
@@ -0,0 +1,28 @@
+settings = {
+	"discord" : {
+			"token": "NzI4OTg4MjIyMjMwNjI2MzEy.XwCZyg.3I5a98uOfsCZvYLir0DJj8NIgt4",
+			"felix_server_id" : 728988042987307091,
+			"prefix" : "феликс "
+		},
+	"database" : {
+			"Emoji" : {
+					729012187300888697 : {
+							"🎮" : 729011989581660160,
+							"🃏" : 729012026646724619,
+						},
+				},
+			"Members" : {
+					427854075099348993 : [ 
+							{"name" : "РАНГ", "prefix" : "#", "val" : 4},
+							{"name" : "ЛВЛ",  "prefix" : "%", "val" : 34},
+							{"name" : "ОПЫТ", "prefix" : "%", "val" : [394,2450]},
+						],
+					249807876309123082 : [
+							{"name" : "PARAM1", "prefix" : "PREFIX1", "val" : "VAL1"},
+							{"name" : "PARAM2", "prefix" : "PREFIX2", "val" : "VAL2"},
+							{"name" : "PARAM3", "prefix" : "PREFIX3", "val" : ["VAL3.1","VAL3.2"]},
+						],
+				},
+		},
+}
+
