9aab91f6127943e41fe3aa32c3730effebb1c11f
Lucky_Cat <60096074+Be3y4uu-K0T@users.noreply.github.com>
2020-07-13 00:08:10 +0500

    ДАТАБАЗА И ЕЁ ВОЗМОЖНОСТИ!!!

--- index.py
+++ index.py
@@ -1,12 +1,60 @@
+from oauth2client.service_account import ServiceAccountCredentials as GoogleAccount
 from discord.ext import commands, tasks
+from settings import settings
+import gspread as gsp
 import discord as ds
+import asyncio
 
 #from urllib.request import urlopen
 #import git
 
-from settings import settings
+bot    = commands.Bot(command_prefix = settings['discord']['prefix'])
+client = gsp.authorize(GoogleAccount.from_json_keyfile_dict(settings['database']['token'],settings['database']['scope']))
+spreadsheet = client.open('DataBase')
 
-bot = commands.Bot(command_prefix = settings['discord']['prefix'])
+
+@bot.event
+async def on_ready():
+    print("[ BOT STARTED ]")
+    
+    global server
+    server = bot.get_guild(settings['discord']['felix_server_id'])
+
+@bot.command(name = 'дай-карточку', aliases = ['дай-карту'])
+async def card(ctx, member: ds.Member = None):
+    if member == None:
+        member = ctx.author
+
+    embed  = ds.Embed(title = 'ЛИЧНАЯ КАРТОЧКА', description = member.top_role.mention, colour = ds.Color.red())
+    embed.set_author(name = member.name, icon_url = member.avatar_url)
+    embed.set_footer(text = bot.user.name, icon_url = bot.user.avatar_url)
+    embed.set_thumbnail(url = member.avatar_url)
+    
+    sheet = spreadsheet.worksheet('Members')
+    for obj in sheet.get_all_records():
+        if obj['Member ID'] == member.id:
+            for args in eval(obj['Data'],{}):
+                embed.add_field(**args)
+            break
+    await ctx.send(embed = embed)
+
+@bot.event
+async def on_raw_reaction_add(ctx):
+    sheet = spreadsheet.worksheet('Emoji')
+    for obj in sheet.get_all_records():
+        if obj['Message ID'] == ctx.message_id and obj['Emoji'] == str(ctx.emoji):
+            await server.get_member(ctx.user_id).add_roles(ds.utils.get(server.roles, id = obj['Role ID']))
+            break
+
+@bot.event
+async def on_raw_reaction_remove(ctx):
+    sheet = spreadsheet.worksheet('Emoji')
+    for obj in sheet.get_all_records():
+        if obj['Message ID'] == ctx.message_id and obj['Emoji'] == str(ctx.emoji):
+            await server.get_member(ctx.user_id).remove_roles(ds.utils.get(server.roles, id = obj['Role ID']))
+            break
+        
+bot.run(settings['discord']['token'])
 
 '''
 @tasks.loop(hours=24)
@@ -19,39 +67,3 @@ async def updates():
     if new_ver > __version__:
         pass #загрузка новой версии с github
 '''
-
-@bot.command(name = 'дай-карточку', aliases = ['дай-карту'])
-async def card(ctx, person: ds.Member = None):
-    if person == None:
-        person = ctx.author
-        
-    embed  = ds.Embed(title = 'ЛИЧНАЯ КАРТОЧКА', description = person.top_role.mention, colour = ds.Color.red())
-    embed.set_author(name = person.name, icon_url = person.avatar_url)
-    embed.set_footer(text = bot.user.name, icon_url = bot.user.avatar_url)
-    embed.set_thumbnail(url = person.avatar_url)
-    if (member := settings['database']['Members'].get(person.id)) != None:
-        for field in member:
-            if type(field['val']) is list:
-                embed.add_field(name = field['name'], value = f"{field['prefix']}{'/'.join(map(str,field['val']))}" )
-            else:
-                embed.add_field(name = field['name'], value = f"{field['prefix']}{field['val']}" )
-    await ctx.send(embed = embed)
-
-@bot.event
-async def on_raw_reaction_add(ctx):
-    if (role := settings['database']['Emoji'].get(ctx.message_id, {}).get(str(ctx.emoji))) != None:
-        await server.get_member(ctx.user_id).add_roles(ds.utils.get(server.roles, id = role))
-
-@bot.event
-async def on_raw_reaction_remove(ctx):
-    if (role := settings['database']['Emoji'].get(ctx.message_id, {}).get(str(ctx.emoji))) != None:
-        await server.get_member(ctx.user_id).remove_roles(ds.utils.get(server.roles, id = role))
-
-@bot.event
-async def on_ready():
-    print("[ BOT STARTED ]")
-    
-    global server
-    server = bot.get_guild(settings['discord']['felix_server_id'])
-
-bot.run(settings['discord']['token'])
