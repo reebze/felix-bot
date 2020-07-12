f67d1bb0c08b8424bd968affe5dd5f24c10debf9
Lucky_Cat <60096074+Be3y4uu-K0T@users.noreply.github.com>
2020-07-13 00:07:01 +0500

    ДАТАБАЗА ДОБАВЛЕНА УРА!!!

--- settings.py
+++ settings.py
@@ -3,26 +3,21 @@ settings = {
 			"token": "NzI4OTg4MjIyMjMwNjI2MzEy.XwCZyg.3I5a98uOfsCZvYLir0DJj8NIgt4",
 			"felix_server_id" : 728988042987307091,
 			"prefix" : "феликс "
-		},
+			},
 	"database" : {
-			"Emoji" : {
-					729012187300888697 : {
-							"🎮" : 729011989581660160,
-							"🃏" : 729012026646724619,
-						},
-				},
-			"Members" : {
-					427854075099348993 : [ 
-							{"name" : "РАНГ", "prefix" : "#", "val" : 4},
-							{"name" : "ЛВЛ",  "prefix" : "%", "val" : 34},
-							{"name" : "ОПЫТ", "prefix" : "%", "val" : [394,2450]},
-						],
-					249807876309123082 : [
-							{"name" : "PARAM1", "prefix" : "PREFIX1", "val" : "VAL1"},
-							{"name" : "PARAM2", "prefix" : "PREFIX2", "val" : "VAL2"},
-							{"name" : "PARAM3", "prefix" : "PREFIX3", "val" : ["VAL3.1","VAL3.2"]},
-						],
-				},
+			"token" : {
+					"type": "service_account",
+  					"project_id": "felix-animation-bot",
+  					"private_key_id": "0dbfcaa962dcca8e2cc3daeda3702421a0a31a0e",
+  					"private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCq+wbJZ0MPQbe4\nXtboRuSFTYrYW7K9mXk5ZMsPrtN0u60EosrCqyePROgHBZoRJkEKmpRV9HRNeITT\n/CZPtJmF/FHDWQAw5vTuZ4qtc64PQv3Q59Xs64uPNtSmRuGbJW/6jCQio1/Nikrg\nbuhGff6nCH4eAxkOWWhF6QfVKPgxBHHfZDeeKvJuEC8FuDVrjrBQtEUSdAOHMBkI\nZEDlNQcvFoSEwPgVzTKnS+IkCnmKK0IfQ0X+S2YIqiQRbmjiK7Levc5allgXvdKD\nUMslPgQiIjDxkb+NZxd8ru/TMA+kffbmI76LQMo4yjji2+fo9mQ7L7UkJc8ltduu\nPCZEooNnAgMBAAECggEAAVsTjF306oEN5jiyTDiLsAGTu/Ag3woBgRKrMt5Nr5uc\nB/EL2eFQ41JGOLqy+bcrmtxA7Ybjs92PHWiJcTuwX/Sb6AhOTRT3v5AzSaQk2YV2\nzRpELahsfIqQbcUayDJ9dyd3UzhSa9HQktkDnyjOmnUhyDZ5aLi4Gp3PbGxHvjO/\nKvu9GxAd29yL9Q4C9qUlwpw06+npz6eTKtrUHCgGOGDCMZvbWRMSPKq4k2Bff8zC\nr99ZP1oMfmY8P4jHUtqEl6SIn0TkVujjXCixZzTqbrcRdIGCqD1MqwhsCH59iOdy\n02Ob3tgn46c078mR9E0P1m1d4Ghg7s1D8mAAWZZtgQKBgQDXc9xUinA8PuUOh/nt\nUYtcBwzSNo1IM11LlEqQpPrp50InTwrHK5z6aD2NSd5H+19WEPCAPeklRt48e606\nWkiYd72B9dIOuTqo1Pv+aC46i66PKHIoSetwvIaplT7AYafvLQEYeDrrIDLzUXK/\nmCLmhTsiMTt9fUVggcSGrGlspwKBgQDLKJR8tYdaZyC095nRjGlEUJEu0U8jpebC\nZ3VVc47TBlIxrY73edPyGCjQSdv1C01oDKnoYfuI869XhiRE4UtR01D1S+HfY32c\ncGYX1Lb27Tw1yFHCFLSDSD3AyEau9tDomor0/8wzl839pdnuafEk9jlkQsI/q6A5\nqu7PQM9LQQKBgBdYMW5jlaehdPYvG5BwPnZZOTVpvcMt9ZTj23Ryu9EuQoe508Qi\n4J5tPwYZE+GexgSwTy7GBoYIeLn6+JkTATyr9LepJ/p6bXWvpiI5rZCgY+iVDFF7\nOOx9MOYdCrCTpDuRF8ZL+K5TTdH5aEKEPHvXwcGBrh15vlOnm+Cvu7iPAoGAME1/\nsH8ApY/riNtt6RK3XJTDUBxX8qPZT9OFZEHLbYUNOd6IJXMD34W6/nrxHzwGtXx9\nUVEqmw9FW0v7h50BaX0SvJ/EHnbrQKzAuV3L+r3BGRB9z+VYXfZpKFL161enqAZG\nj6XaCVJImcw7GqJ5wU4Bq0LsKHNH7y4jBYflQYECgYEAoc02VYQk5L8ZGGbrQax0\nyhLeT+1jCl6FgKtzbmKxkAlWUxkjBZ1dWrC74ZNZ5Ng3GDfHh5LtB0cN8GG6YTZS\n/yrQ6BOYPZJ2U/9EEZJB3Xe7g7Z6WZer0EWoheo0lPTrdTJeIJEmZV7MuMSzoSrj\nGgwg3L015K3EM/TMJ7lmEo0=\n-----END PRIVATE KEY-----\n",
+  					"client_email": "felix-animation-bot@felix-animation-bot.iam.gserviceaccount.com",
+  					"client_id": "108129150669692762022",
+  					"auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  					"token_uri": "https://oauth2.googleapis.com/token",
+  					"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  					"client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/felix-animation-bot%40felix-animation-bot.iam.gserviceaccount.com"
+					},
+			"scope" : ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive'],
+			
 		},
 }
-
