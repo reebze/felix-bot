cd5532fda82154c1f078bdd942597271688cec40
Lucky_Cat <60096074+Be3y4uu-K0T@users.noreply.github.com>
2020-07-14 17:22:59 +0500

    замена google sheets на sqlite3, как датабаза

--- index.py
+++ index.py
@@ -1,26 +1,22 @@
-from oauth2client.service_account import ServiceAccountCredentials as GoogleAccount
 from discord.ext import commands, tasks
-from settings import settings
-import gspread as gsp
 import discord as ds
+import sqlite3
 import asyncio
 
 #from urllib.request import urlopen
 #import git
 
-bot    = commands.Bot(command_prefix = settings['discord']['prefix'])
-client = gsp.authorize(GoogleAccount.from_json_keyfile_dict(settings['database']['token'],settings['database']['scope']))
-spreadsheet = client.open('DataBase')
-
+bot = commands.Bot(command_prefix = "f!")
+con = sqlite3.connect('server.db')
+sql = con.cursor()
 
 @bot.event
 async def on_ready():
-    print("[ BOT STARTED ]")
-    
     global server
-    server = bot.get_guild(settings['discord']['felix_server_id'])
+    server = bot.get_guild(728988042987307091)
+    print("[ BOT STARTED ]")
 
-@bot.command(name = 'дай-карточку', aliases = ['дай-карту'])
+@bot.command()
 async def card(ctx, member: ds.Member = None):
     if member == None:
         member = ctx.author
@@ -29,32 +25,30 @@ async def card(ctx, member: ds.Member = None):
     embed.set_author(name = member.name, icon_url = member.avatar_url)
     embed.set_footer(text = bot.user.name, icon_url = bot.user.avatar_url)
     embed.set_thumbnail(url = member.avatar_url)
-    
-    sheet = spreadsheet.worksheet('Members')
-    for obj in sheet.get_all_records():
-        if obj['Member ID'] == member.id:
-            for args in eval(obj['Data'],{}):
-                embed.add_field(**args)
-            break
+
+    for data in sql.execute("SELECT data FROM users WHERE user_id = ?",(member.id,)):
+        for args in eval(data[0],{'__builtins__':{}}):
+            embed.add_field(**args)
+        break
     await ctx.send(embed = embed)
 
 @bot.event
 async def on_raw_reaction_add(ctx):
-    sheet = spreadsheet.worksheet('Emoji')
-    for obj in sheet.get_all_records():
-        if obj['Message ID'] == ctx.message_id and obj['Emoji'] == str(ctx.emoji):
-            await server.get_member(ctx.user_id).add_roles(ds.utils.get(server.roles, id = obj['Role ID']))
-            break
+    for data in sql.execute("SELECT role_id FROM emojis WHERE msg_id = ? and emoji = ?",(ctx.message_id,ctx.emoji.name)):
+        role = ds.utils.get(server.roles, id = data[0])
+        break
+    member = server.get_member(ctx.user_id)
+    await member.add_roles(role)
+    
 
 @bot.event
 async def on_raw_reaction_remove(ctx):
-    sheet = spreadsheet.worksheet('Emoji')
-    for obj in sheet.get_all_records():
-        if obj['Message ID'] == ctx.message_id and obj['Emoji'] == str(ctx.emoji):
-            await server.get_member(ctx.user_id).remove_roles(ds.utils.get(server.roles, id = obj['Role ID']))
-            break
+    role_id = list(sql.execute("SELECT role_id FROM emojis WHERE msg_id = ? and emoji = ?",(ctx.message_id,ctx.emoji.name)))[0][0]
+    role    = ds.utils.get(server.roles, id = role_id)
+    member  = server.get_member(ctx.user_id)
+    await member.remove_roles(role)
         
-bot.run(settings['discord']['token'])
+bot.run("NzI4OTg4MjIyMjMwNjI2MzEy.XwCZyg.3I5a98uOfsCZvYLir0DJj8NIgt4")
 
 '''
 @tasks.loop(hours=24)
