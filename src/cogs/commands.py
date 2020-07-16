53617b26a9c2e89e194dfaf8633842484c428b60
Lucky_Cat <60096074+Be3y4uu-K0T@users.noreply.github.com>
2020-07-17 02:54:23 +0500

    New commands
    Удаляет сообщения всех или определенного пользователя$ f!clear [количество сообщений] [@упоминание определенного пользователя = по умолчанию все ссобщения]
    Удаляет сообщения пользователя. который вызвал команду$ f!clearself [количество сообщений]

--- src/cogs/commands.py
+++ src/cogs/commands.py
@@ -24,6 +24,7 @@ SOFTWARE.
 """
 #======================================
 from discord.ext import commands
+from datetime import datetime
 from discord import logging
 import discord as ds
 import asyncio
@@ -37,21 +38,53 @@ class CommandsCog(commands.Cog, name = 'Commands'):
 
     def __init__(self, bot):
         self.bot = bot
-        
-    @commands.command()
-    async def test(ctx):
-        print(ctx.channel.history())
-        
+
     @commands.command(name = 'clear')
     @commands.has_permissions(manage_messages=True)
     async def __clear(self, ctx, amount = 5, member: ds.Member = None):
+        if amount <= 0:
+            embed = ds.Embed(color = ds.Color.red(),title = 'Нарываешься?')
+            embed.set_image(url = 'https://media.discordapp.net/attachments/728991407662039140/733423571912753229/flex.png?width=502&height=475')
+            await ctx.channel.send(embed = embed)
+            return 
         if member == None:
-            await ctx.channel.purge(limit = amount)
-            await ctx.guild.get_channel(728991407662039140).send(embed = ds.Embed(color = ds.Color.gold(),title = 'Сообщения удалены!', description=f'В канале {ctx.channel.mention} удалено {amount} сообщений, вызвал {ctx.author.mention}'))
-        #else:
-            
-            #for message in ctx.channel.history():
-                #if message.a
-
+            await ctx.channel.purge(limit = amount + 1) 
+        else:
+            async for message in ctx.channel.history(limit = amount + 1).filter(lambda message: message.author.id == member.id):
+                await message.delete()
+        info_message = await ctx.channel.send(embed = ds.Embed(color = ds.Color.gold(),title = 'Сообщения удалены!', description=f'Удалено {amount} сообщений, вызвал {ctx.author.mention}'))
+        await info_message.add_reaction('🗑️')
+    
+        def check_reaction(reaction, user):
+            return user != ctx.bot.user and str(reaction.emoji) == '🗑️'
+        try:
+            reaction, user = await ctx.bot.wait_for('reaction_add', timeout=10.0, check = check_reaction)
+        except asyncio.TimeoutError:
+            await info_message.remove_reaction('🗑️', ctx.bot.user)
+        else:
+            await info_message.delete()
+    
+    @commands.command(name = 'clearself')
+    async def __clearself(self, ctx, amount = 5):
+        if amount <= 0:
+            embed = ds.Embed(color = ds.Color.red(),title = 'Нарываешься?')
+            embed.set_image(url = 'https://media.discordapp.net/attachments/728991407662039140/733423571912753229/flex.png?width=502&height=475')
+            await ctx.channel.send(embed = embed)
+            return
+        async for message in ctx.channel.history(limit = amount + 1).filter(lambda message: message.author.id == ctx.author.id):
+            await message.delete()
+        info_message = await ctx.channel.send(embed = ds.Embed(color = ds.Color.gold(),title = 'Сообщения удалены!', description=f'В канале {ctx.channel.mention} удалено {amount} сообщений, вызвал {ctx.author.mention}'))
+        await info_message.add_reaction('🗑️')
+    
+        def check_reaction(reaction, user):
+            return user != ctx.bot.user and str(reaction.emoji) == '🗑️'
+        try:
+            reaction, user = await ctx.bot.wait_for('reaction_add', timeout=10.0, check = check_reaction)
+        except asyncio.TimeoutError:
+            await info_message.remove_reaction('🗑️', ctx.bot.user)
+        else:
+            await info_message.delete()
+                
+    
 def setup(bot):
     bot.add_cog(CommandsCog(bot))
